3a7d4e7119bd20730d04cf99fdfb3e0e50b512fc
Sommer <sommekim@mh-hannover.local>
2020-09-03 09:12:50 +0200

    layout changes

--- openehr-sdk-example-app/src/app/formly-types/panel-wrapper.formly.component.ts
+++ openehr-sdk-example-app/src/app/formly-types/panel-wrapper.formly.component.ts
@@ -4,7 +4,7 @@ import { FieldWrapper } from '@ngx-formly/core';
 @Component({
   selector: 'formly-wrapper-panel',
   template: `
-    <div class="card" style="margin-top:10px; border-style:solid; border-width:1px;border-color:LightGrey">
+    <div class="card my-1">
       <div class="card-body">
         <h5 class="card-title">{{ to.label }}</h5>
         <div #fieldComponent></div>
--- openehr-sdk-example-app/src/app/formly-types/repeat-type.formly.component.ts
+++ openehr-sdk-example-app/src/app/formly-types/repeat-type.formly.component.ts
@@ -4,13 +4,13 @@ import { FieldArrayType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-repeat-section',
   template: `
-    <div *ngFor="let field of field.fieldGroup; let i = index;" class="row">
-      <formly-field class="col" [field]="field"></formly-field>
-      <div style="margin:30px 0;">
-        <button class="btn btn-info" type="button" (click)="remove(i)">Remove</button>
+    <div *ngFor="let field of field.fieldGroup; let i = index;" class="card my-1">
+      <div class="card-body d-flex flex-row justify-content-between align-items-center">
+        <formly-field [field]="field"></formly-field>
+        <button class="btn btn-info ml-2" type="button" (click)="remove(i)">Entfernen</button>
       </div>
     </div>
-    <div style="margin:30px 0;">
+    <div class="mt-2">
       <button class="btn btn-info" type="button" (click)="add()">{{ to.addText }}</button>
     </div>
   `,
